425e61e752c132f40e1bee86125382f90a4a1612
Nilesh Vijayrania <nilesh0109@gmail.com>
2019-08-19 17:23:49 +0200

    Adding the results for model trained with L2_regualrized_loss

--- RL-Deep/hw1/train.sh
+++ RL-Deep/hw1/train.sh
@@ -2,6 +2,5 @@
 set -eux
 for e in Hopper-v2 Ant-v2 HalfCheetah-v2 Humanoid-v2 Reacher-v2 Walker2d-v2
 do
-    python main.py expert_data/$e.pkl $e --train --learning_type=1
-    #python main.py expert_data/$e.pkl $e --train --learning_type=0
+    python main.py expert_data/$e.pkl $e --train --learning_type=0
 done
